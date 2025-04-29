a205184e947702f2448bfb84e25a20c492a69728
OliBaise <oli@olibaisedigital.com>
2025-04-29 11:29:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -69,10 +69,10 @@ document.addEventListener("DOMContentLoaded", function () {
   
   <p><strong>Projected House Price (${targetYear}):</strong> $${projectedHousePrice.toLocaleString()}</p>
   
-  <p><strong>Required Deposit (${Math.round(depositPercentage * 100)}% of $${projectedHousePrice.toLocaleString()}) :</strong> $${Math.max(depositNeeded, 0).toLocaleString(undefined, {maximumFractionDigits: 2})}  
+  <p><strong>Required Downpayment (${Math.round(depositPercentage * 100)}% of $${projectedHousePrice.toLocaleString()}) :</strong> $${Math.max(depositNeeded, 0).toLocaleString(undefined, {maximumFractionDigits: 2})}  
   
   <p><strong>Monthly Savings Needed ($${Math.max(depositNeeded, 0).toLocaleString(undefined, {maximumFractionDigits: 2})} ÷ ${monthsToSave}):</strong> $${monthlySavingsNeeded.toLocaleString(undefined, {maximumFractionDigits: 2})} 
-  <br><br>(Calculated as required deposit $${Math.max(depositNeeded, 0).toLocaleString(undefined, {maximumFractionDigits: 2})} ÷ ${monthsToSave} months)</p>
+  <br><br>(Calculated as required downpayment $${Math.max(depositNeeded, 0).toLocaleString(undefined, {maximumFractionDigits: 2})} ÷ ${monthsToSave} months)</p>
   
   <p><strong>Estimated Monthly Mortgage Repayments:</strong> $${monthlyMortgageRepayment.toLocaleString(undefined, {maximumFractionDigits: 2})}  
   <br><br>(Calculated on loan amount $${mortgageAmount.toLocaleString(undefined, {maximumFractionDigits: 2})} over ${mortgageLength} years at ${ (interestRate * 100).toFixed(2) }% interest)</p>
